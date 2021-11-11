0a6dcca8b0bfb3b15f89ddca52ef9ba9f93c3339
VojislavD <vojislavdragicevic17@gmail.com>
2021-11-11 17:06:38 +0100

    Create form

--- resources/views/livewire/contact-form.blade.php
+++ resources/views/livewire/contact-form.blade.php
@@ -1,3 +1,39 @@
 <div>
-    Contact Form Goes Here
+    @if(session('emailSent'))
+        <p>{{ session('emailSent') }}</p>
+    @endif
+
+    @if(session('emailNotSent'))
+        <p>{{ session('emailNotSent') }}</p>
+    @endif
+
+    <form wire:submit.prevent="submit">
+        <div>
+            <label for="name">Name</label>
+            <input type="text" id="name" wire:model="name">
+            @error('name')
+                <span>{{ $message }}</span>
+            @enderror
+        </div>
+
+        <div>
+            <label for="email">Email</label>
+            <input type="text" id="email" wire:model="email">
+            @error('email')
+                <span>{{ $message }}</span>
+            @enderror
+        </div>
+
+        <div>
+            <label for="body">Body</label>
+            <textarea name="body" id="body" wire:model="body"></textarea>
+            @error('body')
+                <span>{{ $message }}</span>
+            @enderror
+        </div>
+
+        <div>
+            <button>Send</button>
+        </div>
+    </form>
 </div>
--- src/Http/Livewire/ContactForm.php
+++ src/Http/Livewire/ContactForm.php
@@ -6,6 +6,31 @@ use Livewire\Component;
 
 class ContactForm extends Component
 {
+    public $name;
+    public $email;
+    public $body;
+
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string'],
+            'email' => ['required', 'email'],
+            'body' => ['required', 'string']
+        ];
+    }
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function submit()
+    {
+        $this->validate();
+
+        dd('submit');
+    }
+
     public function render()
     {
         return view('livewireContactForm::livewire.contact-form');
